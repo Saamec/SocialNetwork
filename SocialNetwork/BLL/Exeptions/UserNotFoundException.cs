4342ff704d924ac7f83dcfe4f6ab2e4d8894c306
«Ilya <333zero777@mail.ru>
2023-01-16 19:04:24 +0300

    Добавьте файлы проекта.

---
+++ SocialNetwork/BLL/Exeptions/UserNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetwork.BLL.Exeptions
+{
+    public class UserNotFoundException : Exception
+    {
+
+    }
+}
---
+++ SocialNetwork/PLL/Views/UserAddFriendsView.cs
@@ -0,0 +1,53 @@
+﻿using SocialNetwork.BLL.Exeptions;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using SocialNetwork.DAL.Entities;
+using SocialNetwork.DAL.Repositories;
+using SocialNetwork.PLL.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetwork.PLL.Views
+{
+    public class UserAddFriendsView 
+    {
+        FriendRepository friendRepository = new FriendRepository();
+        UserService userService;
+        public UserAddFriendsView(UserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public void Show(User user)
+        {
+            Console.Write("Введите почтовый адрес друга, которого хотите добавить:");
+            string emailFriend = Console.ReadLine();
+            bool isFriendExist = false;
+            if (!String.IsNullOrEmpty(emailFriend))
+            {
+                try
+                {
+                    user = userService.FindByEmail(emailFriend);
+                    isFriendExist = true;
+                }
+                catch (UserNotFoundException ex) { Console.WriteLine("Пользователь с такой почтой не найден!"); }
+
+                if (isFriendExist == true)
+                {
+                    FriendEntity friendEntity = new FriendEntity();
+                    friendEntity.friend_id = user.Id;
+                    
+                    friendRepository.Create(friendEntity);
+                    
+                    Console.WriteLine("Друг был успешно добавлен");
+                }
+            } else Console.WriteLine("Вы не ввели значение!");
+            
+            this.userService.Update(user);
+
+            SuccessMessage.Show("Ваш профиль успешно обновлён!");
+        }
+
+    }
+}
